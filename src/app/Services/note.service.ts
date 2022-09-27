a39546c9d9c5ade9a919f0c8854fcae8b968cc90
mananuf <bmananaf@gmail.com>
2022-09-27 13:56:01 +0100

    gets notes from server through getNote()

--- src/app/Services/note.service.ts
+++ src/app/Services/note.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable,of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { NoteInterface } from '../note-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
+  // api url to get notes
   notesURL = "http://localhost:3000/notes"
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  getNotes():Observable<NoteInterface[]>{
+    return this.http.get<NoteInterface[]>(this.notesURL)
+  }
 }
